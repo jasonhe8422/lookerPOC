adc29db8bd0163de122ff9b60318da6079377cba
jasonhe8422 <hhs8422@126.com>
2022-04-28 17:30:45 +0800

    Update highchart-poc.js

--- highchart-poc.js
+++ highchart-poc.js
@@ -17,7 +17,7 @@ const visObject = {
    * data is passed to it.
    **/
   create: function(element, config){
-    element.innerHTML = "<h1>Ready to render!</h1>";
+   // element.innerHTML = "<h1>Ready to render!</h1>";
   },
 
   /**
@@ -29,6 +29,82 @@ const visObject = {
     console.log(JSON.stringify(queryResponse));
     console.log("----------data------------");
     console.log(JSON.stringify(data));
+    let containerId = "container:"+new Date().getTime();
+    element.id= containerId;
+    Highcharts.chart(containerId, {
+      chart: {
+        type: 'spline',
+        scrollablePlotArea: {
+          minWidth: 600,
+          scrollPositionX: 1
+        }
+      },
+      title: {
+        text: 'Wind speed during two days',
+        align: 'left'
+      },
+      subtitle: {
+        text: '13th & 14th of February, 2018 at two locations in Vik i Sogn, Norway',
+        align: 'left'
+      },
+      xAxis: {
+        type: 'datetime',
+        labels: {
+          overflow: 'justify'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Wind speed (m/s)'
+        },
+        minorGridLineWidth: 0,
+        gridLineWidth: 0,
+        alternateGridColor: null,
+      },
+      tooltip: {
+        valueSuffix: ' m/s'
+      },
+      plotOptions: {
+        spline: {
+          lineWidth: 4,
+          states: {
+            hover: {
+              lineWidth: 5
+            }
+          },
+          marker: {
+            enabled: false
+          },
+          pointInterval: 3600000, // one hour
+          pointStart: Date.UTC(2018, 1, 13, 0, 0, 0)
+        }
+      },
+      series: [{
+        name: 'Hestavollane',
+        data: [
+          3.7, 3.3, 3.9, 5.1, 3.5, 3.8, 4.0, 5.0, 6.1, 3.7, 3.3, 6.4,
+          6.9, 6.0, 6.8, 4.4, 4.0, 3.8, 5.0, 4.9, 9.2, 9.6, 9.5, 6.3,
+          9.5, 10.8, 14.0, 11.5, 10.0, 10.2, 10.3, 9.4, 8.9, 10.6, 10.5, 11.1,
+          10.4, 10.7, 11.3, 10.2, 9.6, 10.2, 11.1, 10.8, 13.0, 12.5, 12.5, 11.3,
+          10.1
+        ]
+
+      }, {
+        name: 'Vik',
+        data: [
+          0.2, 0.1, 0.1, 0.1, 0.3, 0.2, 0.3, 0.1, 0.7, 0.3, 0.2, 0.2,
+          0.3, 0.1, 0.3, 0.4, 0.3, 0.2, 0.3, 0.2, 0.4, 0.0, 0.9, 0.3,
+          0.7, 1.1, 1.8, 1.2, 1.4, 1.2, 0.9, 0.8, 0.9, 0.2, 0.4, 1.2,
+          0.3, 2.3, 1.0, 0.7, 1.0, 0.8, 2.0, 1.2, 1.4, 3.7, 2.1, 2.0,
+          1.5
+        ]
+      }],
+      navigation: {
+        menuItemStyle: {
+          fontSize: '10px'
+        }
+      }
+    });
 
 
     doneRendering()
