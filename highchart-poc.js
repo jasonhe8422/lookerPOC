9907d176415880eac8f0b9f00ebc51cbc6d7f2a4
jasonhe8422 <hhs8422@126.com>
2022-05-10 17:28:36 +0800

    Update highchart-poc.js

--- highchart-poc.js
+++ highchart-poc.js
@@ -142,7 +142,7 @@ const visObject = {
     // drillIntoDiv.id = "drillInto-" + new Date().getTime();
     // element.appendChild(drillIntoDiv);
 
-
+    const drillIntoDiv = {};
     // Throw some errors and exit if the shape of the data isn't what this chart needs.
     if (!queryResponse.fields.dimensions || queryResponse.fields.dimensions.length == 0) {
       console.error("This chart requires dimensions.");
