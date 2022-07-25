bafe000577da5d673b7eaa0d114760d104d34f23
jasonhe8422 <hhs8422@126.com>
2022-07-25 18:35:54 +0800

    Update stacked-area.js

--- stacked-area.js
+++ stacked-area.js
@@ -115,8 +115,8 @@ const visObject = {
   updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
     // Clear any errors from previous updates.
     this.clearErrors();
-    // console.log("config: ");
-    // console.log(JSON.stringify(config));
+    console.log("config: ");
+    console.log(JSON.stringify(config));
     // console.log("queryResponse: ");
     // console.log(JSON.stringify(queryResponse));
     // console.log("data: ");
